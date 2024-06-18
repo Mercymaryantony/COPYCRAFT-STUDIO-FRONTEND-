f817f07eb108060cda4754626a63c9437c86fae0
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-18 10:54:41 +0530

    refined

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Navbarcover from './Navbarcover'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div>
-       <div className="container">
+        <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row g-3">
@@ -13,11 +12,15 @@ const Login = () => {
                             <label htmlFor="" className="form-label"><b>USERNAME</b></label>
                             <input type="text" className="form-control" />
                         </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label"><b>PASSWORD</b></label>
+                            <input type="password" name="" id="" className="form-control" />
+                        </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 placeholder-wave">
                             <button className="btn btn-primary">LOGIN</button>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 placeholder-wave">
-                            <p>CLICK HERE TO CREATE A NEW ACCOUNT &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Link to='/signup'><button className="btn btn-success">SIGNUP</button></Link></p>
+                            <p>CLICK HERE TO CREATE A NEW ACCOUNT &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Link to='/signup'><button className="btn btn-success">SIGNUP</button></Link> </p>
                         </div>
                     </div>
                 </div>
