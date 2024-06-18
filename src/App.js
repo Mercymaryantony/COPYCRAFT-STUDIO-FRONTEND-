e9d9d6457e18d380b1bba129e7617ef122d23e3e
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-18 09:16:42 +0530

    login ui done changes are yet to be done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Cover from './components/Cover';
 import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
     <div>
       <Cover/>
       <Signup/>
+      <Login/>
     </div>
   );
 }
