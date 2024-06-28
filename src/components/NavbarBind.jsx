e57da0729164557b04cc98c723f7371c6efd11d1
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-29 00:44:13 +0530

    updated

--- src/components/NavbarBind.jsx
+++ src/components/NavbarBind.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-const Navabar = () => {
+const NavbarBind = () => {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary ">
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary ">
   <div class="container-fluid">
     <Link class="navbar-brand" to="/">COPYCRAFT STUDIO</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,26 +12,28 @@ const Navabar = () => {
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="/Printing">PRINT</Link>
-        </li>
         
-        <li class="nav-item ">
-          <Link class="nav-link" to="/searchprint">SEARCH PRINT</Link>
-        </li>
-       
         <li class="nav-item">
-          <Link class="nav-link" to="/userprintview">USER PRINT VIEW</Link>
+          <Link class="nav-link" to="/Binding">BINDING</Link>
+        </li>
+      
+        <li class="nav-item ">
+          <Link class="nav-link" to="/searchbind">SEARCH BIND</Link>
         </li>
        
+        <li class="nav-item ">
+          <Link class="nav-link" to="/userbindview">USER BIND VIEW</Link>
+        </li>
         
         
       </ul>
     </div>
   </div>
 </nav>
+
+
     </div>
   )
 }
 
-export default Navabar
+export default NavbarBind
--- src/components/UserViewBind.jsx
+++ src/components/UserViewBind.jsx
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import NavbarAdmin from './NavbarAdmin'
+import Navabar from './Navabar'
+import NavbarBind from './NavbarBind'
 
 const UserViewBind = () => {
     const [data, changedata] = useState([])
@@ -21,6 +24,7 @@ const UserViewBind = () => {
 
     return (
         <div>
+            <NavbarBind />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
